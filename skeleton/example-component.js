8d7c5a72f05544dbda328857d2889900078ef1e9
Antti Kontoniemi <antti.kontoniemi@wunderdog.fi>
2019-05-29 23:41:39 +0300

    Change example component class name

--- skeleton/example-component.js
+++ skeleton/example-component.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class ScrollifyList extends Component {
+export default class Example extends Component {
   render() {
     return <h1>Hello React!</h1>
   }
